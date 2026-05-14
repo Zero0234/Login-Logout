12d9ef0ebd041de08f25b098c8eb0a603d4d86f6
Harukiiii <khylselwyn123@gmail.com>
2026-05-14 10:28:45 +0800

    Testing 2nd - Enabled Sharing of SuperUser

--- LoginLogout/settings.py
+++ LoginLogout/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-boqwvdey66+lop*w(n8wfp9gp-d^!h95d!9uuic^kgpoj=-l3@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila'
 
 USE_I18N = True
 
